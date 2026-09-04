352f05b3fc72d052fc042c45fa3c995ba350f074
Matheus Macedo <macedo.matheus0601@gmail.com>
2026-09-03 22:17:52 -0300

    Todas as suites reprovam o build quando falham, com autoteste que garante isso

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Guarda os arquivos do site em cache; os DADOS ficam em localStorage (ver app).
    Chamadas ao Supabase nunca são cacheadas aqui: quando não há rede, o app usa
    o último retrato salvo e entra em modo somente-consulta. */
-const CACHE = "arca-v33";
+const CACHE = "arca-v34";
 const ARQUIVOS = [
   "./",
   "./index.html",
